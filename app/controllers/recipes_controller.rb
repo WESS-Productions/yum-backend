361b77350561d02d0ddd82abdfe94cceece88883
wagnergon <wagnergon52@gmail.com>
2023-05-16 13:25:45 -0700

    changes fixed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,41 +2,41 @@ class RecipesController < ApplicationController
 
   
     def index
-      @recipes = Recipe.all
-      render json: @recipes
+      recipes = Recipe.all
+      render json: recipes
     end
   
     def show
-      render json: @recipe
+      render json: recipes
     end
   
     def create
-      @recipe = Recipe.new(recipe_params)
-      if @recipe.save
-        render json: @recipe, status: :created
+      recipe = Recipe.new(recipe_params)
+      if recipe.valid?
+        render json: apartment
       else
-        render json: @recipe.errors, status: :unprocessable_entity
+        render json: recipe.errors, status: : 422
       end
     end
   
     def update
-      if @recipe.update(recipe_params)
-        render json: @recipe
+      if recipe.update(recipe_params)
+        render json: recipe
       else
-        render json: @recipe.errors, status: :unprocessable_entity
+        render json: recipe.errors, status: : 422
       end
     end
   
     def destroy
-      @recipe.destroy
-      head :no_content
+        recipe = Recipe.find(params[:id])
+        if recipe.destroy
+        render json: recipe
+        else
+        render json: recipe.errors, status: 422
+        end
     end
   
     private
-  
-    def set_recipe
-      @recipe = Recipe.find(params[:id])
-    end
   
     def recipe_params
       params.require(:recipe).permit(:title, :ingredients, :instructions, :vegetarian, :image_url, :cook_time, :prep_time, :user_id)
